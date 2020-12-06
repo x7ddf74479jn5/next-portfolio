a4307e9cc4b7562dbdcbe2d3c0965a040aa7da89
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2020-12-06 22:49:38 +0900

    API test

---
+++ src/pages/api/mail.ts
@@ -0,0 +1,54 @@
+import Cors from "cors";
+import { NextApiRequest, NextApiResponse } from "next";
+
+// // Helper method to wait for a middleware to execute before continuing
+// // And to throw an error when an error happens in a middleware
+// const initMiddleware(middleware) {
+//     return (req, res) =>
+//       new Promise((resolve, reject) => {
+//         middleware(req, res, (result) => {
+//           if (result instanceof Error) {
+//             return reject(result)
+//           }
+//           return resolve(result)
+//         })
+//       })
+//   }
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD", "OPTION"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+//   (req:NextApiRequest,res:NextApiResponse,cors:any)=>any
+
+// // Initialize the cors middleware
+// const cors = initMiddleware(
+//   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+//   Cors({
+//     // Only allow requests with GET, POST and OPTIONS
+//     methods: ['GET', 'POST', 'OPTIONS'],
+//   })
+// )
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Run cors
+  await runMiddleware(req, res, cors);
+
+  // Rest of the API logic
+  res.json({ message: "Hello Everyone!" });
+}
