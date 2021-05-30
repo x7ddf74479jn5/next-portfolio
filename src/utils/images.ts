7f3e92bdc8e7095cf84f5d6a7456c643dbdd5ad8
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-05-31 01:46:22 +0900

    modify: add SVG Icon

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,18 +1,24 @@
+import { identity } from "./helper";
+
 type Image = {
   [key: string]: { path: string; alt: string };
 };
 
-export const images: Image = {
+export const images: Image = identity({
   pandasharkIcon: { path: "/img/icons/pandashark_icon.png", alt: "Pandashark's icon" },
   pandasharkRectangle: { path: "/img/icons/pandashark_logo_rectangle.webp", alt: "Pandashark's icon" },
   ec: { path: "/img/samples/ec-app-eyecatch.webp", alt: "eye catch of EC app" },
   chat: { path: "/img/samples/chat-bot-eyecatch.webp", alt: "eye catch chat" },
   portfolio: { path: "/img/samples/portfolio-eyecatch.webp", alt: "eye catch portfolio" },
-  twitter: { path: "/img/icons/twitter_logo.png", alt: "twitter" },
-  robot: { path: "/img/icons/robot.png", alt: "chat bot" },
   websiteProduction: { path: "/img/services/website-production.jpeg", alt: "Website Production" },
   wordPress: { path: "/img/services/wordpress-icon.png", alt: "WordPress" },
   webApp: { path: "/img/services/web-app.jpeg", alt: "Chat APP" },
   favicon: { path: "/img/icons/favicon.ico", alt: "" },
   appleTouchIcon: { path: "/img/icons/apple-touch-icon.png", alt: "" },
-} as const;
+});
+
+export const icons = identity({
+  twitter: { path: "/img/icons/twitter-logo.svg", alt: "twitter" },
+  github: { path: "/img/icons/github-logo.svg", alt: "github" },
+  chatBot: { path: "/img/icons/question_answer_black.svg", alt: "chatBot" },
+});
