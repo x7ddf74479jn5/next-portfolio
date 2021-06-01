b94a0e6e5da556395f362aa7896073138108ce08
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-06-01 23:12:44 +0900

    fix: zod

--- src/types/api/index.ts
+++ src/types/api/index.ts
@@ -1,19 +1,10 @@
-import * as z from "zod";
+import { z } from "zod";
 
 export const schema = z.object({
-  name: z.string().nonempty({ message: "お名前が未入力です。" }),
-  email: z
-    .string()
-    .email({ message: "メールアドレスの形式に誤りがあります。" })
-    .nonempty({ message: "メールアドレスが未入力です。" }),
-  category: z.union([
-    z.literal(""),
-    z.literal("Webサイト制作について"),
-    z.literal("Webアプリ開発について"),
-    z.literal("Webサイト模写について"),
-    z.literal("その他"),
-  ]),
-  description: z.string().nonempty({ message: "お問い合わせ内容が未入力です。" }),
+  name: z.string().min(1, "お名前が未入力です。"),
+  email: z.string().email({ message: "メールアドレスの形式に誤りがあります。" }).min(1, "メールアドレスが未入力です。"),
+  category: z.string(),
+  description: z.string().min(1, "お問い合わせ内容が未入力です。"),
 });
 
 export type FormData = z.infer<typeof schema>;
