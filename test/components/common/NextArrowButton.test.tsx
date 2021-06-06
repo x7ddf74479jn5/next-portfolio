34b80d99554a587a29e53e1c2000ef042401a42d
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-06-06 21:58:40 +0900

    add: NextArrowButton test

--- test/components/common/NextArrowButton.test.tsx
+++ test/components/common/NextArrowButton.test.tsx
@@ -6,10 +6,6 @@ describe("NextArrowButton", () => {
     const { asFragment } = render(<NextArrowButton href="/" />, {});
     expect(asFragment()).toMatchSnapshot();
   });
-  it("matches snapshot", () => {
-    const { asFragment } = render(<NextArrowButton href="/" onDark />, {});
-    expect(asFragment()).toMatch;
-  });
   it("Next Link component compiles is transformed the anchor element with appropriate href", () => {
     const { container } = render(<NextArrowButton href="/" />, {});
     expect(container.firstChild).toHaveAttribute("href", "/");
