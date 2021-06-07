c23cc06dbcddfda1582733cb188ffe300a61ee36
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-06-07 23:11:16 +0900

    add: Career test

---
+++ test/components/about/Career.test.tsx
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import Career from "src/components/about/Career";
+
+describe("Career", () => {
+  it("matches snapshot", () => {
+    const { asFragment } = render(<Career />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("shows headline and has id", () => {
+    const { container } = render(<Career />);
+    const targetElement = container.getElementsByClassName("section")[0];
+    expect(container).toHaveTextContent("Career");
+    expect(targetElement).toHaveAttribute("id", "career");
+  });
+
+  it("timeline lists have over one item", () => {
+    const { container } = render(<Career />);
+    const targetElement = container.getElementsByClassName("timeline");
+    expect(targetElement.length).toBeGreaterThan(0);
+  });
+});
