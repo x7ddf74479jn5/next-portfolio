444b63ff59e2c74f3cd629d3937095ef15c88631
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-06-12 01:41:49 +0900

    chore: Spacer props fix

--- src/components/layouts/Spacer.tsx
+++ src/components/layouts/Spacer.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import styles from "src/styles/components/layouts/Spacer.module.scss";
 
 type Props = {
-  size: "xxxs" | "xxs" | "xs" | "sm" | "md" | "lg" | "xl";
+  size?: "xxxs" | "xxs" | "xs" | "sm" | "md" | "lg" | "xl";
 };
 
 const Spacer: React.VFC<Props> = ({ size = "md" }) => {
