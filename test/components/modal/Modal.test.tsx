246122d6a98bc747981f0f670a896369b990f661
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-06-16 21:20:05 +0900

    wip: modified Modal text

--- test/components/modal/Modal.test.tsx
+++ test/components/modal/Modal.test.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
+import type Queries from "@testing-library/dom/types/queries";
 import type { RenderResult } from "@testing-library/react";
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
@@ -6,44 +6,60 @@ import ModalPortal from "src/components/modal/Modal";
 import { ModalDispatchContext, ModalStateContext } from "src/context/ModalProviderContainer";
 import type { State } from "src/hooks/useModalCore";
 
+const initialState: State = {
+  isModalOpen: false,
+  modalType: undefined,
+  data: null,
+};
+
+const mockState = (state?: State): State => {
+  return { ...initialState, ...state };
+};
+
+const mockDispatchContextValue = {
+  openModal: jest.fn(),
+  closeModal: jest.fn(),
+};
+
+const customRender = (ui: React.ReactElement, state?: State): RenderResult<typeof Queries, HTMLElement> => {
+  const mockStateContextValue = mockState(state);
+  return render(
+    <>
+      <div id="__next" />
+      <ModalStateContext.Provider value={mockStateContextValue}>
+        <ModalDispatchContext.Provider value={mockDispatchContextValue}>{ui}</ModalDispatchContext.Provider>
+      </ModalStateContext.Provider>
+    </>
+  );
+};
+
 describe("ModalPortal", () => {
   let renderResult: RenderResult;
+  let newState: State;
 
-  const initialState: State = {
-    isModalOpen: true,
-    modalType: "DRAWER",
-    data: null,
-  };
-
-  const mockDispatchContextValue = {
-    openModal: () => {},
-    closeModal: () => {},
-  };
-
-  const Providers: React.ComponentType<{ children?: any }> = ({ children }) => {
-    return (
-      <ModalStateContext.Provider value={initialState}>
-        <ModalDispatchContext.Provider value={mockDispatchContextValue}>{children}</ModalDispatchContext.Provider>
-      </ModalStateContext.Provider>
-    );
-  };
-
-  beforeEach(() => {
-    renderResult = render(
-      <>
-        <div id="__next" />
-        <Providers>
-          <ModalPortal />
-        </Providers>
-      </>
-    );
+  it("initial render matches snapshot", () => {
+    renderResult = customRender(<ModalPortal />);
+    expect(renderResult.asFragment()).toMatchSnapshot();
   });
 
-  it("matches snapshot", () => {
+  it("Drawer modal matches snapshot", () => {
+    newState = {
+      isModalOpen: true,
+      modalType: "DRAWER",
+      data: null,
+    };
+    renderResult = customRender(<ModalPortal />, newState);
     expect(renderResult.asFragment()).toMatchSnapshot();
   });
 
   it("closes modal when backdrop is clicked", () => {
+    newState = {
+      isModalOpen: true,
+      modalType: "DRAWER",
+      data: null,
+    };
+    renderResult = customRender(<ModalPortal />, newState);
+
     const backdrop = renderResult.container.getElementsByClassName("backdrop_70")[0];
     expect(backdrop).toBeInTheDocument();
     expect(renderResult.container.childElementCount).toBeGreaterThan(0);
