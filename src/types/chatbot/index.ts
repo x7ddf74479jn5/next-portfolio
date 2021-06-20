90e7cebfb89a9966d50fbed2dc822e7ab9bcba7c
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-06-21 00:29:59 +0900

    add: types of chatbot

---
+++ src/types/chatbot/index.ts
@@ -0,0 +1,40 @@
+export interface Dataset {
+  init: ChatData;
+  job_offer: ChatData;
+  website: ChatData;
+  webapp: ChatData;
+  wordpress: ChatData;
+  other_jobs: ChatData;
+  about: ChatData;
+  job: ChatData;
+  favorite: ChatData;
+  sns: ChatData;
+  detail: ChatData;
+  menu: ChatData;
+  recipe_sites: ChatData;
+  japanese: ChatData;
+  jp_simple: ChatData;
+  jp_normal: ChatData;
+  western: ChatData;
+  we_simple: ChatData;
+  we_normal: ChatData;
+  chinese: ChatData;
+  ch_simple: ChatData;
+  ch_normal: ChatData;
+  other: ChatData;
+  [k: string]: ChatData;
+}
+
+export interface ChatData {
+  answers: Answer[];
+  question: string;
+}
+
+export interface Answer {
+  content: string;
+  nextId: string;
+}
+
+export type NextQuestionId = keyof Dataset | "contact";
+
+export type Chat = { text: string; type: "question" | "answer" };
