c21110b08248aeb07bb204fa184055d3bbde6d9a
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-06-23 16:12:46 +0900

    　modify: layer property for FromModalDaigalog

--- src/components/chatbot/FormModalContainer.tsx
+++ src/components/chatbot/FormModalContainer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FormDialog from "src/components/chatbot/FormDialog";
 import Backdrop from "src/components/modal/Backdrop";
-import styles from "src/styles/components/modal/ModalContainer.module.scss";
+import styles from "src/styles/components/chatbot/FormModalContainer.module.scss";
 
 type Props = {
   handleClose: () => void;
