d5e7e7e6e2642857fe91c8c4211bc92165d20675
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-06-25 23:40:43 +0900

    chore

--- test/hooks/useBodyLockScroll.test.ts
+++ test/hooks/useBodyLockScroll.test.ts
@@ -2,16 +2,18 @@ import { renderHook } from "@testing-library/react-hooks";
 
 import useBodyLockScroll from "../../src/hooks/useBodyLockScroll";
 
-test("evaluates media query", () => {
-  window.getComputedStyle = jest.fn().mockReturnValue({ overflow: "test" });
-  document.body.style.overflow = "";
+describe("useBodyLockScroll", () => {
+  test("evaluates media query", () => {
+    window.getComputedStyle = jest.fn().mockReturnValue({ overflow: "test" });
+    document.body.style.overflow = "";
 
-  const { unmount } = renderHook(() => {
-    return useBodyLockScroll();
+    const { unmount } = renderHook(() => {
+      return useBodyLockScroll();
+    });
+
+    expect(window.getComputedStyle).toHaveBeenCalled();
+
+    unmount();
+    expect(document.body.style.overflow).toBe("test");
   });
-
-  expect(window.getComputedStyle).toHaveBeenCalled();
-
-  unmount();
-  expect(document.body.style.overflow).toBe("test");
 });
--- test/hooks/useMedia.test.ts
+++ test/hooks/useMedia.test.ts
@@ -2,23 +2,25 @@ import { renderHook } from "@testing-library/react-hooks";
 
 import useMedia from "../../src/hooks/useMedia";
 
-test("evaluates media query", () => {
-  window.addEventListener = jest.fn();
-  window.removeEventListener = jest.fn();
+describe("useMedia", () => {
+  test("evaluates media query", () => {
+    window.addEventListener = jest.fn();
+    window.removeEventListener = jest.fn();
 
-  window.matchMedia = jest.fn().mockImplementation(() => {
-    return {
-      matches: false,
-    };
+    window.matchMedia = jest.fn().mockImplementation(() => {
+      return {
+        matches: false,
+      };
+    });
+
+    const { result, unmount } = renderHook(() => {
+      return useMedia();
+    });
+
+    expect(result.current.isPC).toBe(false);
+    expect(window.addEventListener).toHaveBeenCalled();
+
+    unmount();
+    expect(window.removeEventListener).toHaveBeenCalled();
   });
-
-  const { result, unmount } = renderHook(() => {
-    return useMedia();
-  });
-
-  expect(result.current.isPC).toBe(false);
-  expect(window.addEventListener).toHaveBeenCalled();
-
-  unmount();
-  expect(window.removeEventListener).toHaveBeenCalled();
 });
