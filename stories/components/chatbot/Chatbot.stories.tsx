d713063e7e8027d1bf98c1d88ab9fce7683c46b6
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-06-27 20:32:59 +0900

    chore: storyshots

--- stories/components/chatbot/Chatbot.stories.tsx
+++ stories/components/chatbot/Chatbot.stories.tsx
@@ -1,10 +1,18 @@
 import type { Meta } from "@storybook/react";
+import type { StoryFnReactReturnType } from "@storybook/react/dist/ts3.9/client/preview/types";
 import React from "react";
 import Chatbot from "src/components/chatbot/Chatbot";
+
+import { withContext } from ".storybook/context";
 
 export default {
   title: "chatbot/Chatbot",
   component: Chatbot,
+  decorators: [
+    (storyFn: () => StoryFnReactReturnType) => {
+      return withContext(storyFn);
+    },
+  ],
 } as Meta;
 
 export const chatbot = () => {
