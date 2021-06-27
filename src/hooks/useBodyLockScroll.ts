814b1670f1c4811dc52a8238b625e94bc3a6ccb8
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-06-27 21:41:19 +0900

    fix: useBodyLockScroll

--- src/hooks/useBodyLockScroll.ts
+++ src/hooks/useBodyLockScroll.ts
@@ -5,7 +5,7 @@ const useLockBodyScroll = () => {
     const originalStyle = window.getComputedStyle(document.body).overflow;
     document.body.style.overflow = "hidden";
     return () => {
-      return (document.body.style.overflow = originalStyle);
+      document.body.style.overflow = originalStyle;
     };
   }, []);
 };
