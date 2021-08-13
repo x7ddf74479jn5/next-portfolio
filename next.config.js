c3841dea0077ffa04f8b0b787e0ddc2416cc90e4
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-08-14 03:07:58 +0900

    update: node_modules

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const path = require("path");
 
 module.exports = {
-  future: { webpack5: true },
   i18n: { locales: ["ja"], defaultLocale: "ja" },
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
