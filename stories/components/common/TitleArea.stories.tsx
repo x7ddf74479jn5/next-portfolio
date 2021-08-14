ca972380dad5871164c57330a6d67fe0c46800b5
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-08-14 20:09:10 +0900

    fix: broken tests by upgrade

--- stories/components/common/TitleArea.stories.tsx
+++ stories/components/common/TitleArea.stories.tsx
@@ -1,21 +1,25 @@
 import type { Meta, Story } from "@storybook/react";
+import type { StoryFnReactReturnType } from "@storybook/react/dist/ts3.9/client/preview/types";
 import type { ComponentProps } from "react";
 import { TitleArea } from "src/components/common/TitleArea";
-import { withNextRouter } from "storybook-addon-next-router";
+
+import { withRouterContext } from "../../../.storybook/context";
 
 type Props = ComponentProps<typeof TitleArea>;
 
+const routerOptions = {
+  route: "/",
+  pathname: "/samples/portfolio",
+  asPath: "/samples/portfolio",
+};
 export default {
   title: "common/TitleArea",
   component: TitleArea,
-  decorators: [withNextRouter],
-  parameters: {
-    nextRouter: {
-      route: "/",
-      pathname: "/samples/portfolio",
-      asPath: "/samples/portfolio",
+  decorators: [
+    (storyFn: () => StoryFnReactReturnType) => {
+      return withRouterContext(storyFn, routerOptions);
     },
-  },
+  ],
 } as Meta;
 
 const Template: Story<Props> = (args) => {
